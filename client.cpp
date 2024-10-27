f973269c4c3ff49eeac8a2e09178a1e345803e3e
Ai3011 <161729375+Ai3011@users.noreply.github.com>
2024-10-27 09:07:01 -0400

    Update client.cpp
    update client (Теперь работает постояно)

--- client.cpp
+++ client.cpp
@@ -1,46 +1,58 @@
-#include <iostream> 
-#include <cstring> 
-#include <arpa/inet.h> 
-#include <unistd.h> 
+#include <iostream>   
+#include <cstring>   
+#include <arpa/inet.h>   
+#include <unistd.h>   
  
-using namespace std; 
+using namespace std;   
  
-int main() { 
-    int sock = 0; 
-    struct sockaddr_in serv_addr; 
+int main() {   
+    int sock = 0;   
+    struct sockaddr_in serv_addr;   
  
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) { 
-        cout << "Socket creation error" << endl; 
-        return -1; 
-    } 
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {   
+        cout << "Socket creation error" << endl;   
+        return -1;   
+    }   
  
-    serv_addr.sin_family = AF_INET; 
-    serv_addr.sin_port = htons(7432); 
+    serv_addr.sin_family = AF_INET;   
+    serv_addr.sin_port = htons(7432);   
  
-    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) { 
-        cout << "Invalid address/ Address not supported" << endl; 
-        return -1; 
-    } 
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {   
+        cout << "Invalid address/ Address not supported" << endl;   
+        return -1;   
+    }   
  
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) { 
-        cout << "Connection failed" << endl; 
-        return -1; 
-    } 
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {   
+        cout << "Connection failed" << endl;   
+        return -1;   
+    }   
  
-    string command; 
-    cout << "Enter your command (e.g., SELECT, INSERT): "; 
-    getline(cin, command); 
+    while (true) {   
+        string command;   
+        cout << "Enter your command (e.g., SELECT, INSERT, EXIT to quit): ";   
+        getline(cin, command);   
  
-    send(sock, command.c_str(), command.length(), 0); 
+        // Проверка на команду завершения   
+        if (command == "EXIT") {   
+            cout << "Exiting client..." << endl;   
+            break;   
+        }   
  
-    char buffer[1024] = {0}; 
-    int bytes_received = read(sock, buffer, sizeof(buffer)); 
-    if (bytes_received > 0) { 
-        cout << "Response from server: " << buffer << endl; 
-    } else { 
-        cout << "Failed to receive response" << endl; 
-    } 
+        // Отправка команды серверу   
+        send(sock, command.c_str(), command.length(), 0);   
  
-    close(sock); 
-    return 0; 
+        // Очистка буфера перед чтением 
+        char buffer[1024] = {0};   
+         
+        // Получение ответа от сервера   
+        int bytes_received = read(sock, buffer, sizeof(buffer));   
+        if (bytes_received > 0) {   
+            cout << "Response from server: " << buffer << endl;   
+        } else {   
+            cout << "Failed to receive response" << endl;   
+        }   
+    }   
+ 
+    close(sock);   
+    return 0;   
 }
